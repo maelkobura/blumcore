0a933f75ba90ef7d3f2dc3696c49c8d88012c621
maelb <eyeznbk@gmail.com>
2025-08-28 19:16:37 +0200

    misc annotation for database

---
+++ api/src/main/java/blum/api/annotation/Identifier.java
@@ -0,0 +1,8 @@
+package blum.api.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Identifier {
+}
